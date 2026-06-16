e60f72e5d9afd2153ad30cc71e0b1987d2fd797d
PhamLeNhatHoang <nhathoang1470@gmail.com>
2026-06-16 17:02:40 +0700

    fix login gg

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -54,7 +54,7 @@ export class AuthController {
   @Get("google/callback")
   async googleCallback(@Req() rq, @Res() res) {
     const response = await this.authService.login(rq.user.id);
-    res.redirect(`http://localhost:5173/?accessToken=${response.accessToken}`);
+    res.redirect(`https://cho-tot-production.up.railway.app/?accessToken=${response.accessToken}`);
   }
 
   
