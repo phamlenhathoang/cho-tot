a137d84e963ce38d62a173f7f040da3d718196a3
PhamLeNhatHoang <nhathoang1470@gmail.com>
2026-06-21 11:56:48 +0700

    add getAll in Order

--- src/module/twilio/twilio.controller.ts
+++ src/module/twilio/twilio.controller.ts
@@ -19,7 +19,7 @@ export class TwilioController {
 
   @Post("send-mail-otp")
   async sendOtpMail(@Body() sendMailOtpDTO : SendMailOtpDto) {
-    return await this.twilioService.sendOtpMail(sendMailOtpDTO.email, sendMailOtpDTO.otp)
+    return await this.twilioService.sendOtpMail(sendMailOtpDTO.email, sendMailOtpDTO.newPassword)
   }
   
 }
